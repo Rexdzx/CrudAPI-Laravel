08c4414ed2feec661e9b08d8672d306794eab47a
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-01-18 14:36:51 +0700

    edit tables

--- database/migrations/2023_01_18_070230_create_posts_table.php
+++ database/migrations/2023_01_18_070230_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->text('content');
             $table->string('slug');
             $table->smallInteger('status');
