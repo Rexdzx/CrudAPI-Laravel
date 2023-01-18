a7ea41a9821b1886893b7855ffaa4498c42c61d8
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-01-18 14:20:15 +0700

    create resource

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'slug' => $this->slug,
+            'status' => $this->status,
+            'create_at' => $this->create_at,
+            'update_at' => $this->update_at
+        ];
+    }
+}
